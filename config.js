13fe330aa2c66a5f4b6266253beea68af34028f9
Julian <joticajulian@gmail.com>
2019-08-14 01:33:29 +0200

    new categories

--- config.js
+++ config.js
@@ -2,19 +2,17 @@ const DB_FILENAME = 'db.json'
 const STATE_FILENAME = 'state.json'
 
 const CATEGORY = {
-  entradas: 'Entradas',
-  basicos: 'Servicios basicos',
-  supermercado: 'Supermercado',
-  fuera: 'Comida fuera',
-  transporte: 'Transporte',
-  salud: 'Salud',
-  otros: 'Otros',
-  movimientos: 'Movimientos',
-  inversion: 'Inversion',
-  educacion: 'Educacion',
-  vacaciones: 'Vacaciones',
-  legal: 'Legal',
-  efectivo: 'Efectivo',
+  entradas: 'Entradas', // salarios ... subvenciones ... reembolso ... otras entradas
+  basicos: 'Servicios basicos', // alquiler y servicios
+  supermercado: 'Supermercado', // alimentacion y hogar
+  fuera: 'Comida fuera', // restaurantes y salidas
+  transporte: 'Transporte', // transporte
+  salud: 'Salud', // salud y bienestar
+  otros: 'Otros', // otros gastos ... shopping
+  movimientos: 'Movimientos', //
+  inversion: 'Inversion', //
+  educacion: 'Educacion', //
+  efectivo: 'Efectivo', // retiro de dinero
 }
 
 module.exports = {
