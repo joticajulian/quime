2221d4543f4e8bbcd44d5a46e678cb0c8d4130c9
Julian <joticajulian@gmail.com>
2019-08-14 07:39:32 +0200

    accounts for actives, pasives, incomes, liabilities

--- config.js
+++ config.js
@@ -1,18 +1,30 @@
 const DB_FILENAME = 'db.json'
 const STATE_FILENAME = 'state.json'
 
-const CATEGORY = {
-  entradas: 'Entradas', // salarios ... subvenciones ... reembolso ... otras entradas
-  basicos: 'Servicios basicos', // alquiler y servicios
-  supermercado: 'Supermercado', // alimentacion y hogar
-  fuera: 'Comida fuera', // restaurantes y salidas
-  transporte: 'Transporte', // transporte
-  salud: 'Salud', // salud y bienestar
-  otros: 'Otros', // otros gastos ... shopping
-  movimientos: 'Movimientos', //
-  inversion: 'Inversion', //
-  educacion: 'Educacion', //
-  efectivo: 'Efectivo', // retiro de dinero
+const ACCOUNTS = {
+  // actives and pasives
+  bank1: 'Banco 1',
+  bank2: 'Banco 2',
+  deuda1: 'Deuda 1',
+  deuda2: 'Deuda 2',
+  cash1: 'Efectivo 1',
+  cash2: 'Efectivo 2',
+  inversion1: 'Inversion 1',
+  inversion2: 'Inversion 2',
+
+  // incomes and liabilities
+  salarios: 'Salarios',
+  subvenciones: 'Subvenciones',
+  reembolsos: 'Reembolsos',
+  otras_entradas: 'Otras entradas',
+  alquiler: 'Alquiler y servicios',
+  alimentacion: 'Alimentacion y hogar',
+  restaurantes: 'Restaurantes y salidas',
+  transporte: 'Transporte',
+  salud: 'Salud y bienestar',
+  shopping: 'Shopping',
+  otros_gastos: 'Otros gastos',
+  educacion: 'Educacion',
 }
 
 module.exports = {
