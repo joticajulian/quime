88f876ae0bdce7164d53138f7f46f9cfa37ecd68
Julian Gonzalez <joticajulian@gmail.com>
2019-08-16 18:32:08 +0200

    reportspage list incomes expenses

--- src/views/ReportsPage.vue
+++ src/views/ReportsPage.vue
@@ -1,10 +1,10 @@
-<template>
+﻿<template>
   <div>
     <div class="container row mt-5">
       <div class="col-md-2">
         <div class="form-group row">
           <label class="col-4">Año</label>
-          <select class="col-8 mb-2">
+          <select class="col-8 mb-2" v-model="selection.year">
             <option value="2018">2018</option>
             <option value="2019">2019</option>
             <option value="2020">2020</option>
@@ -13,11 +13,35 @@
         </div>
         <div class="form-group row">
           <label class="col-4">Mes</label>
-          <select class="col-8 mb-3">
+          <select class="col-8 mb-3" v-model="selection.month">
             <option v-for="(month,index) in months" :key="index" :value="index">
               {{month}}
             </option>
           </select>
+        </div>
+      </div>
+      <div class="col-md-10">
+        <div class="row">
+          <div class="col-6">
+            <h4>Gastos</h4>
+            <div class="card mb-4">
+              <ul class="list-group list-group-flush">
+                <li v-for="(item,index) in expenses" class="list-group-item">
+                  {{item.account}}
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-6">
+            <h4>Ingresos</h4>
+            <div class="card mb-4">
+              <ul class="list-group list-group-flush">
+                <li v-for="(item,index) in incomes" class="list-group-item">
+                  {{item.account}}
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -45,11 +69,62 @@ export default{
         'Octubre',
         'Noviembre',
         'Diciembre'
-      ]
+      ],
+      selection: {
+        year: '2019',
+        month: '0'
+      },
+      assets: [],
+      liabilities: [],
+      incomes: [],
+      expenses: [],
+    }
+  },
+  watch: {
+    'selection.year': function(){
+      console.log(this.selection.year)
+      this.loadReport( n*12 + m + 1 )
+    },
+    'selection.month': function(){
+      console.log(this.months[this.selection.month])
+      var n = this.selection.year - 2018
+      var m = this.selection.month
+      this.loadReport( n*12 + m + 1 )
     }
   },
   created(){
     console.log(state)
   },
+  methods: {
+    loadReport(){
+      var n = parseInt(this.selection.year) - 2018
+      var m = parseInt(this.selection.month)
+      var index = n*12 + m +1
+      
+      this.incomes = []
+      this.expenses = []
+      this.assets = []
+      this.liabilities = []
+      for(var i in state.balances_by_period[index].accounts){
+        var balance = state.balances_by_period[index].accounts[i]
+        switch(balance.account_type){
+          case 'asset':
+            this.assets.push(balance)
+            break
+          case 'liability':
+            this.liabilities.push(balance)
+            break
+          case 'income':
+            this.incomes.push(balance)
+            break
+          case 'expense':
+            this.expenses.push(balance)
+            break
+          default:
+            throw new Error(`Cannot recognize account type '${balance.account_type}'`)
+        }
+      }
+    },
+  }
 }
 </script>
