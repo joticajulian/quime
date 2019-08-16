f7daa7c4123eaedaec686214a2a3a9f42fb8aca3
Julian Gonzalez <joticajulian@gmail.com>
2019-08-16 17:34:56 +0200

    initial page for reports

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import ReportsPage from './views/ReportsPage.vue'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'reports',
+      component: ReportsPage
     },
     {
       path: '/about',
---
+++ src/views/ReportsPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="container row mt-5">
+      <div class="col-md-2">
+        <div class="form-group row">
+          <label class="col-4">Año</label>
+          <select class="col-8 mb-2">
+            <option value="2018">2018</option>
+            <option value="2019">2019</option>
+            <option value="2020">2020</option>
+            <option value="2021">2021</option>
+          </select>
+        </div>
+        <div class="form-group row">
+          <label class="col-4">Mes</label>
+          <select class="col-8 mb-3">
+            <option v-for="(month,index) in months" :key="index" :value="index">
+              {{month}}
+            </option>
+          </select>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import state from '@/../state.json'
+import db from '@/../db.json'
+
+export default{
+  name: 'ReportsPage',
+  data(){
+    return {
+      months:[
+        'Enero',
+        'Febrero',
+        'Marzo',
+        'Abril',
+        'Mayo',
+        'Junio',
+        'Julio',
+        'Agosto',
+        'Septiembre',
+        'Octubre',
+        'Noviembre',
+        'Diciembre'
+      ]
+    }
+  },
+  created(){
+    console.log(state)
+  },
+}
+</script>
