076f063629c1d7e24939bb476919fa1b28ee0cd5
Julian Gonzalez <joticajulian@gmail.com>
2019-09-15 04:33:21 +0200

    config server

--- config.js
+++ config.js
@@ -1,11 +1,15 @@
 const DB_FILENAME = 'db.json'
 const STATE_FILENAME = 'state.json'
 const PUBLIC_ROOT = './dist'
-const USERNAME = 'user'
-const PASSWORD = 'pass'
+const PRIVATE_ROOT = './'
+const USERNAME = process.env.USERNAME_QUIME || 'user'
+const PASSWORD = process.env.PASSWORD || 'pass'
 
 module.exports = {
   DB_FILENAME,
   STATE_FILENAME,
   PUBLIC_ROOT,
+  PRIVATE_ROOT,
+  USERNAME,
+  PASSWORD
 }
