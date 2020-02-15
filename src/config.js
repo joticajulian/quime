24f0469900b58dc81dc3ff4e2c7244af34f4bad7
Julian <joticajulian@gmail.com>
2020-02-15 07:01:34 +0100

    fix config vue production env

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 var SERVER = 'http://localhost:3000/'
 var SERVER_API = 'http://localhost:3000/api/'
 
-if(process.env.PRODUCTION){
+if(process.env.NODE_ENV === "production"){
   var SERVER = '/'
   var SERVER_API = '/api/'
 }
